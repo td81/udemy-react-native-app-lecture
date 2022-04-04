22238dfd4b13112f50920cf59fee7c284ba2f824
td81 <cre556@gmail.com>
2022-04-04 19:00:45 +0900

    learn Image add

--- news-app/App.js
+++ news-app/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -29,7 +29,12 @@ export default function App() {
   return (
     <View style={styles.container}>
         <View style={styles.itemContainer}>
-          <View style={styles.leftContainer} />
+          <View style={styles.leftContainer}>
+            <Image
+              style={{width: 100, height: 100}}
+              source={{uri: 'https://i.picsum.photos/id/211/200/200.jpg?hmac=VJ4wl95YuQJMvM_1O83L3nSfTn20OxaVfWe0wNMZrIc'}}
+            />
+          </View>
           <View style={styles.rightConteiner} />
         </View>
     </View>
