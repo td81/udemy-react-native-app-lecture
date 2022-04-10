bafbeee38dc44b8267bfb14c37ed1301aa2b0a34
td81 <cre556@gmail.com>
2022-04-10 12:35:24 +0900

    learn ReactNavi

--- news-app/screens/ArticleScreen.js
+++ news-app/screens/ArticleScreen.js
@@ -9,14 +9,15 @@ const styles = StyleSheet.create({
     },
 });
 
+//iframeタグではなくスマホアプリの場合は<WebView source={{uri: article.uri}} />　としたいが、
+//PCのブラウザでは動作しないためiframeタグを使っている(iframeの場合、サーバ側のセキュリティ設定の都合上表示されないが)
+//https://qiita.com/shunp110/items/246c727bdb546120e006
 export default function ArticleScreen( { route }) {
     const { article } = route.params;
-    console.log(article)
+    console.log(article.url)
     return(
-        <SafeAreaView style={styles.container}>     
-            <WebView
-            source={{uri: article.uri}}
-            />
+        <SafeAreaView style={styles.container}> 
+            <iframe src={article.url} style={styles} /> 
         </SafeAreaView>
     );
 }
